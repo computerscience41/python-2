e4d04f2f907a86a8d1d7cad812d5852a01f98bf7
computerscience41 <rakibhasan934541@gmail.com>
2024-11-16 16:31:40 +0600

    Operations

---
+++ operation.py
@@ -0,0 +1,10 @@
+# Basic Math operations
+
+a = 11
+b = 4
+
+print("Addition:", a + b)
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Division:", a / b)
+print("Modulus:", a % b)
